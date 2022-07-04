52be35c1ae8778a1c7b11c1e4aeea87ffa6cb49e
kospov <popov.k.g@gmail.com>
2022-07-04 15:06:05 +0000

    Deploying to gh-pages from @ kospov/goit-js-hw-09@4a95f78c9d61b4b9f1623f7a58abc738c973fa12 🚀

---
+++ 01-color-switcher.f315934d.js
@@ -0,0 +1,2 @@
+!function(){const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let d=null;function n(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}e.setAttribute("disabled",!1),t.addEventListener("click",(function(){d=setInterval(n,1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(d),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.f315934d.js.map
