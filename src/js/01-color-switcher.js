4a95f78c9d61b4b9f1623f7a58abc738c973fa12
Konstantin Popov <popov.k.g@gmail.com>
2022-07-04 18:05:44 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const startBtnEl = document.querySelector("[data-start]");
+const stopBtnEl = document.querySelector("[data-stop]");
+let timerId = null;
 
+stopBtnEl.setAttribute('disabled', false);
+
+startBtnEl.addEventListener('click', onStartBntClick);
+
+stopBtnEl.addEventListener('click', onStopBtnClick);
+
+function onStartBntClick() {
+    timerId = setInterval(changeBodyBackgroundColor, 1000);
+    startBtnEl.disabled = true;
+    stopBtnEl.disabled = false;
+}
+
+function onStopBtnClick() {
+    clearInterval(timerId);
+    startBtnEl.disabled = false;
+    stopBtnEl.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBodyBackgroundColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+}
